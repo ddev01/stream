17978079f5a4a1729ea810b6aebed49f28ebebf1
ddev01 <71857399+ddev01@users.noreply.github.com>
2025-11-04 03:04:02 +0100

    refactor(models): remove email from User model
    Remove email from fillable attributes and email_verified_at from casts
    since email is no longer stored in the users table.

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,6 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
         'password',
     ];
 
@@ -44,7 +43,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
