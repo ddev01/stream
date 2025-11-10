a9c0505e1babff2bf87f5693122abb89c52f8a18
ddev01 <71857399+ddev01@users.noreply.github.com>
2025-11-10 07:21:56 +0100

    feat(routes): add raid history API endpoint
    Register POST /api/twitch/raid-history route with dev.api middleware
    for secure bulk import of raid history data.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\RaidHistoryController;
 use App\Http\Controllers\Api\SubscriptionHistoryController;
 use App\Http\Controllers\Api\TwitchStatsController;
 use Illuminate\Http\Request;
@@ -12,3 +13,4 @@ Route::get('/user', function (Request $request) {
 // Twitch data ingestion endpoints (protected by API token or dev key)
 Route::post('/twitch/stats', [TwitchStatsController::class, 'bulkImport'])->middleware('dev.api');
 Route::post('/twitch/subscription-history', [SubscriptionHistoryController::class, 'bulkImport'])->middleware('dev.api');
+Route::post('/twitch/raid-history', [RaidHistoryController::class, 'bulkImport'])->middleware('dev.api');
